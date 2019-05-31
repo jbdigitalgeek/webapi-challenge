0f6ecca51f8b997f8ad769f9adfc5e7b447b9602
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-31 09:40:51 -0700

    inital get function working in server.js and wired up

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`You're in the root directory`);
+})
 
 module.exports = server;
